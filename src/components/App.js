837bef094c900b92db17daa11513322d32a80f51
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-29 11:23:08 +0100

    Allow to use hooks with functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  return <div className="ui container">App</div>;
+  const [resource, setResource] = useState("posts");
+
+  return (
+    <div className="ui container">
+      <div>
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
+      </div>
+
+      {resource}
+    </div>
+  );
 };
 
 export default App;
